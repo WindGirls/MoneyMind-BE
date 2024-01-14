0801db23302c4110bf1b9cc6c4f0f5053e06684a
Jeongeun Lee <2jeongeun7@gmail.com>
2024-01-14 20:48:48 +0900

    chore: 프로젝트 초기 설정

---
+++ src/main/java/WindGirls/MoneyMinder/MoneyMinderApplication.java
@@ -0,0 +1,13 @@
+package WindGirls.MoneyMinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyMinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyMinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/WindGirls/MoneyMinder/MoneyMinderApplicationTests.java
@@ -0,0 +1,13 @@
+package WindGirls.MoneyMinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyMinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
